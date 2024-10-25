7d8a829d533a76deb375d845b8711e5ba5bfbbac
Mehnazar <161054226+Mehnazar@users.noreply.github.com>
2024-10-25 23:54:10 +0500

    final commit

--- app/FAQ/page.tsx
+++ app/FAQ/page.tsx
@@ -12,32 +12,32 @@ export default function FAQ(){
             <p className=" text-5x1 font-bold py-6">FAQ</p>
             
             
-      <li>Question: "What is Artificial Intelligence (AI)?",</li>
+      <li>Question: What is Artificial Intelligence (AI)?</li>
       <p>answer:
-        "Artificial Intelligence refers to the simulation of human intelligence in machines that are programmed to think, learn, and perform tasks typically requiring human cognition.",
+        Artificial Intelligence refers to the simulation of human intelligence in machines that are programmed to think, learn, and perform tasks typically requiring human cognition.
         </p>
     
-      <li>Question: "What are the types of AI?", </li>
+      <li>Question: What are the types of AI? </li>
     <p>
       answer:
-        "AI can be broadly categorized into three types: Narrow AI (specific tasks), General AI (human-level intelligence), and Superintelligent AI (beyond human intelligence).",
+        AI can be broadly categorized into three types: Narrow AI (specific tasks), General AI (human-level intelligence), and Superintelligent AI (beyond human intelligence).
         </p>
     
-      <li> Question: "What are common applications of AI?",</li>
+      <li> Question: What are common applications of AI?</li>
       <p>
         answer:
-        "AI is used in many areas, including virtual assistants, self-driving cars, facial recognition, recommendation systems, medical diagnosis, and natural language processing.",
+        AI is used in many areas, including virtual assistants, self-driving cars, facial recognition, recommendation systems, medical diagnosis, and natural language processing.
         </p>
     
-      <li>Question: "Is AI safe?",</li>
+      <li>Question: Is AI safe?</li>
       <p>
       answer:
-        "AI offers many benefits but also presents challenges. Ethical considerations, biases, and misuse must be addressed to ensure AI is developed and used responsibly.",
+        AI offers many benefits but also presents challenges. Ethical considerations, biases, and misuse must be addressed to ensure AI is developed and used responsibly.
         </p>
-      <li> Question: "How does AI learn?",</li>
+      <li> Question: How does AI learn?</li>
       <p>
       answer:
-        "AI systems learn through algorithms and data. Machine learning, a subset of AI, allows machines to improve their performance by analyzing data patterns and making predictions.",
+        AI systems learn through algorithms and data. Machine learning, a subset of AI, allows machines to improve their performance by analyzing data patterns and making predictions.
         </p>
             
           </div>
